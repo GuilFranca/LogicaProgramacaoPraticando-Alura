ed499a3b76b2227a02b07696c66acb1833b235c8
Guilherme França <guilfranca27@gmail.com>
2024-12-20 23:09:36 -0300

    Aula 1 Projeto Sorteador de números - 13 Mão na massa: proteção na entrada de valores

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -7,6 +7,11 @@ function sortear() {
     let sorteados = [];
     let numero;
 
+    if (de >= ate) {
+        alert('Campo "Do número" deve ser inferior ao campo "Até o número". Verifique!');
+        return;
+    }
+
     for (i = 0;i < quantidade; i++) {
         numero = obterNumeroAleatorio(de, ate);
 
