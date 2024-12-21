a9286b3ae71af471e569a24107a49cc2ce2d9266
Guilherme França <guilfranca27@gmail.com>
2024-12-20 22:54:05 -0300

    Aula 1 Projeto Sorteador de números - 10 Botão de reiniciar

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -24,10 +24,25 @@ function sortear() {
     // Pode ser utilizado .textContent para mudar um texto simples dentro de uma tag.
     // Utilizamod o innerHTML para colocar um trecho de código HTML dentro da div.
     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+
+    // Chamar um metódo para alterar o botão reset
+    let botao = document.getElementById('btn-reiniciar');
+    botao.classList.remove('container__botao-desabilitado');
+    botao.classList.add('container__botao');
 }
 
 function obterNumeroAleatorio(min, max) {
     // Math.floor serve para arredondar o número para baixo - 4.3 = 4 / 4.7 = 4.
     return Math.floor(Math.random() * (max - min + 1)) + min;
     // (entre 1 e 0 * (10 - 5 + 1)) + 5.
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    resultado.innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    let botao = document.getElementById('btn-reiniciar');
+    botao.classList.remove('container__botao');
+    botao.classList.add('container__botao-desabilitado');
 }
