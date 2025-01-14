551e55a2c8e1893554b12eb96395bce7fa372a40
GuilFranca <guilfranca27@gmail.com>
2025-01-14 12:53:40 -0300

    Aula 4 Projeto Ingresso Online - Realizando compra tipo pista

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -1,4 +1,19 @@
 function comprar() {
     let tipo = document.getElementById('tipo-ingresso').value;
-    let qtd = document.getElementById('qtd').value;
+    let qtd = parseInt(document.getElementById('qtd').value);
+
+    if (tipo == 'pista') {
+        comprarPista(qtd);
+    }
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para tipo Pista');
+    } else {
+        alert('Compra concluída');
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+    }
 }
