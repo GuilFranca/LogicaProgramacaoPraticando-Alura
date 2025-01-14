a3426198effff9f7ffbb0ba3a2420f68ac7d9b5c
GuilFranca <guilfranca27@gmail.com>
2025-01-14 13:25:00 -0300

    Aula 4 Projeto Ingresso Online - Faça como eu fiz: outros tipos de ingressos

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -4,7 +4,13 @@ function comprar() {
 
     if (tipo == 'pista') {
         comprarPista(qtd);
+    } else if (tipo == 'superior') {
+        comprarSuperior(qtd);
+    } else if (tipo == 'inferior') {
+        comprarInferior(qtd);
     }
+
+    document.getElementById('qtd').value = '';
 }
 
 function comprarPista(qtd) {
@@ -15,5 +21,27 @@ function comprarPista(qtd) {
         alert('Compra concluída');
         qtdPista = qtdPista - qtd;
         document.getElementById('qtd-pista').textContent = qtdPista;
+    }
+}
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdSuperior) {
+        alert('Quantidade indisponível para tipo Superior');
+    } else {
+        alert('Compra concluída');
+        qtdSuperior = qtdSuperior - qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+    }
+}
+
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdInferior) {
+        alert('Quantidade indisponível para tipo Inferior');
+    } else {
+        alert('Compra concluída');
+        qtdInferior = qtdInferior - qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
     }
 }
